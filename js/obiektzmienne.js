78ac968b87bc07de47cff4b22782771f5fa2b0b3
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-10 11:36:13 +0200

    coomit

---
+++ js/obiektzmienne.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+
+var student = {
+    imie: "Kaja",
+    wzrost: 152,
+    wyswietlDane: function () {
+        console.log(this.imie)
+    }
+}
+
+student.wyswietlDane();
+console.log(student);
